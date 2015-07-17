0ed06c31b04bc8314cf0eed5cbc8a4cd37a25f94
alistairfisher <alistairstfisher@gmail.com>
2015-07-17 14:28:43 +0100

    Changed networking parameter from udpv4 to stackv4 Added start function Various syntactic tidying

--- dhcp/dhcp_serverv4.ml
+++ dhcp/dhcp_serverv4.ml
@@ -15,6 +15,19 @@
 
 *)
 
+(*TESTING CHANGES
+
+* Removed redundant exception
+* Added t to GC
+* removed parameters from input
+* used client hardware address and make sure to extract from packet properly
+* new implementations for bottom 3 functions: copy/paste...
+
+*)
+
+
+
+
 (* Permission to use, copy, modify, and distribute this software for any
 * purpose with or without fee is hereby granted, provided that the above
 * copyright notice and this permission notice appear in all copies.
@@ -28,13 +41,15 @@
 * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 *)
 
+
+(*Testing: creating packets, receiving packets, state of the list etc, lease expiration. Print all received state.*)
+
 open Lwt.Infix;;
 open Printf;;
 
-module Make (Console:V1_LWT.CONSOLE)
-  (Time:V1_LWT.TIME)
+module Main (Console:V1_LWT.CONSOLE)
   (Clock:V1.CLOCK)
-  (Udp:V1_LWT.UDPV4) = struct
+  (Stack:V1_LWT.STACKV4) = struct
 
   type reserved_address = {
     ip_address: Ipaddr.V4.t;
@@ -52,14 +67,14 @@ module Make (Console:V1_LWT.CONSOLE)
     client_ip_address: Ipaddr.V4.t;
   }
     
-  type t = { (*not sure if this is appropriate*)
+  type t = {
     c: Console.t;
-    udp: Udp.t;
-    mac: Macaddr.t;
+    stack: Stack.t;
+    reserved_addresses:(string*reserved_address) list ref;
+    in_use_addresses:(clientID*in_use_address) list ref;
+    available_addresses: Ipaddr.V4.t list ref;
   }
-    
-  exception No_server_ID of string;;
-    
+
   cstruct dhcp {
     uint8_t op;
     uint8_t htype;
@@ -88,6 +103,21 @@ module Make (Console:V1_LWT.CONSOLE)
     let b = Ipaddr.V4.to_int32 top in
     if a>b then []
     else bottom::list_gen(Ipaddr.V4.of_int32(Int32.add a Int32.one),top);;
+  
+
+  let rec parameter_request c_requests s_parameters = match c_requests with
+    |[]->[]
+    |(h::t) -> List.assoc h s_parameters :: (parameter_request t s_parameters);;
+
+
+  let make_options_with_lease ~client_requests ~server_parameters ~serverIP ~lease_length ~message_type =
+    let open Dhcpv4_option.Packet in
+    (*let params = parameter_request ~c_requests:client requests ~s_parameters:parameters_list in*)
+    { op = message_type; opts= [`Lease_time lease_length;`Server_identifier serverIP;`End]};;
+    
+  let make_options_no_lease ~client_requests ~server_parameters ~serverIP ~message_type =
+    let open Dhcpv4_option.Packet in
+    {op = message_type;opts = [`Server_identifier serverIP;`End]};;
     
   let output_broadcast t ~xid ~ciaddr ~yiaddr ~siaddr ~giaddr ~secs ~chaddr ~flags ~options =
     let options = Dhcpv4_option.Packet.to_bytes options in
@@ -106,8 +136,7 @@ module Make (Console:V1_LWT.CONSOLE)
     set_dhcp_siaddr buf (Ipaddr.V4.to_int32 siaddr);
     set_dhcp_giaddr buf (Ipaddr.V4.to_int32 giaddr);
     (* TODO add a pad/fill function in cstruct *)
-    let macaddr = Macaddr.to_bytes t.mac in
-    set_dhcp_chaddr (macaddr ^ (Bytes.make 10 '\000')) 0 buf;
+    set_dhcp_chaddr chaddr 0 buf; (*TODO: check this*)
     set_dhcp_sname (Bytes.make 64 '\000') 0 buf; (*check these 2*)
     set_dhcp_file (Bytes.make 128 '\000') 0 buf;
     set_dhcp_cookie buf 0x63825363l;
@@ -119,10 +148,13 @@ module Make (Console:V1_LWT.CONSOLE)
     let buf = Cstruct.set_len buf (sizeof_dhcp + options_len) in
       Console.log_s t.c (sprintf "Sending DHCP packet (length %d)" total_len)
       >>= fun () ->
-      Udp.write ~dest_ip: dest_ip_address ~source_port:67 ~dest_port:68 t.udp buf;;
+        Stack.UDPV4.write ~dest_ip: dest_ip_address ~source_port:67 ~dest_port:68 (Stack.udpv4 t.stack) buf;;
+  
+  let add_address address list = 
+    list:=address::(!list)
   
   (*unwrap DHCP packet, case split depending on the contents*)
-  let input t ~src ~dst:_ ~srcport:_ ~reserved_addresses ~in_use_addresses ~available_addresses ~serverIP ~max_lease ~server_parameters buf = (*lots of duplication with client, need to combine into one unit*)
+  let input t ~serverIP ~max_lease ~server_parameters ~src:_ ~dst:_ ~src_port:_ buf = (*lots of duplication with client, need to combine into one unit*)
     let ciaddr = Ipaddr.V4.of_int32 (get_dhcp_ciaddr buf) in
     let yiaddr = Ipaddr.V4.of_int32 (get_dhcp_yiaddr buf) in
     let siaddr = Ipaddr.V4.of_int32 (get_dhcp_siaddr buf) in
@@ -149,13 +181,12 @@ module Make (Console:V1_LWT.CONSOLE)
       [ "DHCP response:";
         sprintf "input ciaddr %s yiaddr %s" (Ipaddr.V4.to_string ciaddr) (Ipaddr.V4.to_string yiaddr);
         sprintf "siaddr %s giaddr %s" (Ipaddr.V4.to_string siaddr) (Ipaddr.V4.to_string giaddr);
-        sprintf "chaddr %s sname %s file %s" (chaddr) (copy_dhcp_sname buf) (copy_dhcp_file buf) ]
+        sprintf "chaddr %s sname %s file %s" (chaddr) (copy_dhcp_sname buf) (copy_dhcp_file buf)]
     >>= fun () ->
     let open Dhcpv4_option.Packet in
-    let open Dhcp_serverv4_options in
     let client_identifier = match find packet (function `Client_id id -> Some id |_ -> None) with
       |None -> chaddr
-      |Some id-> id
+      |Some id-> Console.log t.c (sprintf "Client identifer set to %s" id);id
     in
     let lease_length = match find packet (function `Lease_time requested_lease -> Some requested_lease |_ -> None) with
       |None -> max_lease
@@ -168,29 +199,34 @@ module Make (Console:V1_LWT.CONSOLE)
     match packet.op with
       |`Discover ->
         let address = match find packet (function `Requested_ip requested_address -> Some requested_address | _ -> None) with
-          |None-> List.hd !available_addresses
+          |None-> List.hd !(t.available_addresses)
           |Some requested_address ->
-            if List.mem requested_address !available_addresses then requested_address
-            else List.hd !available_addresses
+            if List.mem requested_address !(t.available_addresses) then requested_address
+            else List.hd !(t.available_addresses)
         in
-        reserved_addresses:=(client_identifier,{ip_address=address;xid=xid;reservation_timestamp=Clock.time()})::!reserved_addresses;
-        let address_filter f = (f=address)
-        in
-        available_addresses:=List.filter address_filter !available_addresses;
-        let options = make_options_lease ~client_requests: client_requests ~server_parameters:server_parameters ~serverIP: serverIP ~lease_length:lease_length ~message_type:`Offer in
+        Console.log t.c (sprintf "Packet is a discover, currently %d reserved addresses" (List.length !(t.reserved_addresses)));
+        Console.log t.c (sprintf "Allocating %s" (Ipaddr.V4.to_string address));
+        let new_reservation = client_identifier,{ip_address=address;xid=xid;reservation_timestamp=Clock.time()} in
+        add_address new_reservation t.reserved_addresses;
+        Console.log t.c (sprintf "Now %d reserved addresses" (List.length !(t.reserved_addresses)));
+        let address_filter f = (f=address) in
+        t.available_addresses:=List.filter address_filter !(t.available_addresses);
+        let options = make_options_with_lease ~client_requests: client_requests ~server_parameters:server_parameters ~serverIP: serverIP ~lease_length:lease_length ~message_type:`Offer in
         (*send DHCP Offer*)
         output_broadcast t ~xid:xid ~ciaddr:0 ~yiaddr:address ~siaddr:serverIP ~giaddr:giaddr ~secs:secs ~chaddr:chaddr ~flags:flags ~options:options;
       |`Request ->
+        Console.log t.c (sprintf "Packet is a request");
         (match (find packet(function `Server_identifier id ->Some id |_ -> None)) with
           |Some id -> (*This is a true request, client has no IP address*)
             let server_identifier = id
             in
-            if (server_identifier=serverIP && ((List.assoc client_identifier !reserved_addresses).xid=xid)) then ( (*the client is requesting the IP address, this is not a renewal. Need error handling*)
-              let address = (List.assoc client_identifier !reserved_addresses).ip_address in
+            Console.log t.c (sprintf "True request");
+            if (server_identifier=serverIP && ((List.assoc client_identifier !(t.reserved_addresses)).xid=xid)) then ( (*the client is requesting the IP address, this is not a renewal. Need error handling*)
+              let address = (List.assoc client_identifier !(t.reserved_addresses)).ip_address in
               let new_reservation = {identifier=client_identifier;client_ip_address=address},{lease_length=lease_length;lease_timestamp=Clock.time()} in
-              in_use_addresses:=new_reservation::!in_use_addresses;
-              reserved_addresses:=List.remove_assoc client_identifier !reserved_addresses;
-              let options = make_options_lease ~client_requests: client_requests ~server_parameters:server_parameters ~serverIP: serverIP ~lease_length:lease_length ~message_type:`Ack in
+              add_address new_reservation t.in_use_addresses;
+              t.reserved_addresses:=List.remove_assoc client_identifier !(t.reserved_addresses);
+              let options = make_options_with_lease ~client_requests: client_requests ~server_parameters:server_parameters ~serverIP: serverIP ~lease_length:lease_length ~message_type:`Ack in
               output_broadcast t ~xid:xid ~ciaddr:ciaddr ~yiaddr:address ~siaddr:serverIP ~giaddr:giaddr ~secs:secs ~chaddr:chaddr ~flags:flags ~options:options;
             )
             else Lwt.return_unit;
@@ -200,24 +236,29 @@ module Make (Console:V1_LWT.CONSOLE)
             else (*client in renew or rebind*)
               Lwt.return_unit;)
       |`Decline ->
-        if (List.mem_assoc client_identifier !reserved_addresses) then
-          let address = (List.assoc chaddr !reserved_addresses).ip_address in
-          in_use_addresses:=({identifier="Unknown";client_ip_address=address}, {lease_length=Int32.max_int;lease_timestamp=Clock.time()})::!in_use_addresses; (*must notify network admin*)
-          reserved_addresses:=List.tl(!reserved_addresses); Lwt.return_unit;
-        else Lwt.return_unit;
+        (try
+          let address = (List.assoc chaddr !(t.reserved_addresses)).ip_address in
+          let new_reservation = {identifier="Unknown";client_ip_address=address}, {lease_length=Int32.max_int;lease_timestamp=Clock.time()} in
+          add_address new_reservation t.in_use_addresses; (*must notify network admin*)
+          t.reserved_addresses:=List.remove_assoc chaddr !(t.reserved_addresses);
+          Lwt.return_unit;
+        with
+          |Not_found -> Lwt.return_unit;)
       |`Release -> (*this may give errors with duplicate packets, should wipe ALL entries*)
         let entry = {identifier=client_identifier;client_ip_address=ciaddr} in
-        if (List.mem_assoc entry !in_use_addresses) then (
-          available_addresses:=ciaddr::!available_addresses;
-          in_use_addresses:=List.remove_assoc entry !in_use_addresses);
+        Console.log t.c (sprintf "Packet is a release");
+        if (List.mem_assoc entry !(t.in_use_addresses)) then (
+          add_address ciaddr t.available_addresses;
+          t.in_use_addresses:=List.remove_assoc entry !(t.in_use_addresses));
           Lwt.return_unit;
       |`Inform ->
         let options = make_options_no_lease ~client_requests:client_requests ~serverIP:serverIP ~server_parameters:server_parameters ~message_type:`Ack in
         output_broadcast t ~xid:xid ~ciaddr:ciaddr ~yiaddr:yiaddr ~siaddr:serverIP ~giaddr:giaddr ~secs:secs ~chaddr:chaddr ~flags:flags ~options:options;
       | _ ->Lwt.return_unit;; (*this is a packet meant for a client*)
       
-  let rec garbage_collect ~reserved_addresses ~in_use_addresses ~collection_interval=
-    let rec gc_reserved = function
+  let rec garbage_collect t collection_interval=
+    Console.log t.c (sprintf "GC running!");
+    let rec gc_reserved l = match (l) with
       |[] -> []
       |h::t -> if (Clock.time()-.(snd h).reservation_timestamp > collection_interval) then (gc_reserved t)
         else (gc_reserved t)
@@ -227,10 +268,22 @@ module Make (Console:V1_LWT.CONSOLE)
       |h::t -> 
         let lease = Int32.to_int((snd h).lease_length) in
         if (lease != 0xffffffff && (int_of_float(Clock.time()-.(snd h).lease_timestamp) > lease)) then gc_in_use t else h::(gc_in_use t)
-    in Time.sleep(collection_interval)>>=fun()->(reserved_addresses:=(gc_reserved !reserved_addresses));(in_use_addresses:=(gc_in_use !in_use_addresses));garbage_collect ~reserved_addresses:reserved_addresses ~in_use_addresses:in_use_addresses ~collection_interval:collection_interval;;
-        
-    (*let dhcp ~scopebottom ~scopetop ~lease_length ~serverIP ~serverName ~udp ~probe:true ~subnetmask:NONE ~DNSservers:NONE ~ ~= (*note: lease time is in seconds. 0xffffffff is reserved for infinity*)
-      let reserved_addresses:(string*reserved_address) list ref = ref [] in
-      let in_use_addresses:(string*in_use_address) list ref = ref [] in
-    	let available_addresses = ref listgen(scopebottom,scopetop) in*)
+    in OS.Time.sleep(collection_interval)>>=fun()->(t.reserved_addresses:=(gc_reserved !(t.reserved_addresses)));(t.in_use_addresses:=(gc_in_use !(t.in_use_addresses)));
+      garbage_collect t collection_interval;;
+
+  let rec serverThread t lease_length serverIP server_parameters=
+    Stack.listen_udpv4 (t.stack) 67 (input t ~serverIP:serverIP ~max_lease:lease_length ~server_parameters:server_parameters);
+    serverThread t lease_length serverIP server_parameters;;
+    
+  let start ~c ~clock ~stack= (*note: lease time is in seconds. 0xffffffff is reserved for infinity*)
+    let scopebottom = Ipaddr.V4.unspecified in
+    let scopetop = Ipaddr.V4.of_string_exn("0.0.0.100") in
+    let lease_length  = Int32.of_int(3000) in
+    let serverIP = Ipaddr.V4.of_string_exn("172.17.26.57") in
+    let server_parameters = [] in
+    let reserved_addresses = ref [] in
+    let in_use_addresses= ref [] in
+    let available_addresses = ref (list_gen (scopebottom,scopetop)) in
+    let t = {c;stack;reserved_addresses;in_use_addresses;available_addresses} in
+    Lwt.join([serverThread t lease_length serverIP server_parameters;garbage_collect t 60.0]);; 
 end
--- dhcp/dhcp_serverv4_config_parser.ml
+++ dhcp/dhcp_serverv4_config_parser.ml
@@ -2,29 +2,36 @@ open Core.Std
 
 type parameters =
 {
-  default_lease_length: int32 ref;
-  max_lease_length: int32 ref;
-  scope_bottom: Ipaddr.V4.t ref;
-  scope_top: Ipaddr.V4.t ref;
+  default_lease_length: int32 option ref;
+  max_lease_length: int32 option ref;
+  scope_bottom: Ipaddr.V4.t option ref;
+  scope_top: Ipaddr.V4.t option ref;
   parameters: Dhcp_v4_options.t list ref;
 }
 
 type server_parameters =
 {
   globals: parameters;
-  subnets: Ipaddr.V4.t*Ipaddr.V4.t*parameters
+  subnets: (Ipaddr.V4.t*Ipaddr.V4.t*parameters) list ref;
 }
 
 let read_DHCP_config = 
   let input_channel = open_in "/etc/dhcpd.conf" in
   read_globals input_channel;;
 
+let cut_at_semicolon string =
+  try
+    let semicolon_position = String.index string ';' in
+    String.sub string 0 semicolon_position
+  with String.Not_found -> string;;
+ 
 let rec read_globals input_channel =
 
   try
-    let next_line = input_line input_channel in
+    let next_line = cut_at_semicolon(input_line input_channel) in
     let tokens = Str.split " +" input_line in
     let first_token = List.nth tokens 0 in
+    let value = List.nth tokens 1 in
     match first_token with
     |"#" -> read_globals input_channel(*comment:ignore line*)
     |"subnet" ->
@@ -38,15 +45,26 @@ let rec read_globals input_channel =
       current_options.parameters := new_option::(!(current_options.globals.parameters));
       current_options
     |"range" ->
-      let scope_bottom = List.nth tokens 1 in
-      let scope_top = List.nth tokens 2 in
+      let scope_bottom = Some (List.nth tokens 1) in
+      let scope_top = Some (List.nth tokens 2) in
       let current_options = read_globals input_channel in
       current_options.globals.scope_bottom := scope_bottom;
       current_options.globals.scope_top := scope_top;
       current_options
-    |"default_lease_length" 
+    |"default_lease_length" ->
+      let lease_length = Int32.of_string value in
+      let current_options = read_globals input_channel in
+      currentoptions.globals.default_lease_length := lease_length;
+      current_options
+    |"max_lease_length" ->
+      let lease_length = Int32.of_string value in
+      let current_options = read_globals input_channel in
+      currentoptions.globals.max_lease_length := lease_length;
+      current_options
     
-  with End_of_file -> ()
+  with
+  |End_of_file ->
+    {globals = {default_lease_length=ref None;max_lease_length = ref None;scope_bottom = ref None;scope_bottom = ref None;parameters = ref []};subnets=ref[]}
 
 let strings_to_ip_addresses list =
   map (Ipaddrv4.of_string) list;;
@@ -62,35 +80,71 @@ let read_option tokens =
     |"broadcast-address"->`Broadcast (Ipaddrv4.of_string value)
     |"time-offset"->`Time_offset value
     |"routers"->`Router (strings_to_ip_addresses (List.tl tokens))
-    |"ntp-servers"->`Time_server (strings_to_ip_addresses (List.tl tokens)) (*list TODO*)
+    |"ntp-servers"->`Time_server (strings_to_ip_addresses (List.tl tokens))
     |"name-server"->`Name_server (strings_to_ip_addresses (List.tl tokens))
     |"domain-name-servers"->`DNS_server (strings_to_ip_addresses (List.tl tokens))
     |"netbios-name-servers"->`Netbios_name_server (strings_to_ip_addresses (List.tl tokens))
     |"domain-name" -> `Domain_name value
-    |"default-lease-time" -> `Lease_time (Int32.of_string value) (*need to build new data structure*)
+    |"default-lease-time" -> `Lease_time (Int32.of_string value)
     |"max-lease-time" -> `Lease_time (Int32.of_string value)
-    |_-> raise Unknown_option value
+    |_-> raise Unknown_option value;;
 
-exception parsing_error of string (*TODO: add line numbers*)
+exception Parsing_error of string;; (*TODO: add line numbers*)
+exception Undefined_range_for_subnet of string;;
 
 let read_subnet_start tokens input_channel global_list =
   let subnet = Ipaddrv4.of_string(List.hd tokens) in
   match (List.nth 1 tokens),(List.nth 3 tokens) with
   |"netmask","{" ->
-    let netmask = Ipaddrv4.of_string (List.nth 2) in
+    (let netmask = Ipaddrv4.of_string (List.nth 2) in
     let parameters = read_subnet input_channel in
-    {subnet;netmask;parameters}
+    (*check that scope is defined*)
+    match !(parameters.scope_bottom) with
+      |None-> raise undefined_range_for_subnet (List.hd tokens)
+      |Some x->  subnet,netmask,parameters)
   |"netmask",_ -> raise parsing_error "{ expected after subnet declaration"
   |_,_ -> raise parsing_error "netmask not declared in subnet declaration"
   
 
 let rec read_subnet input_channel=
-  let next_line = input_line input_channel in
-  let tokens = Str.split " +" input_line in
+  let next_line = cut_at_semicolon(input_line input_channel) in
+  let tokens = Str.split " +" next_line in
   let first_token = List.hd tokens in
-  let value = List.nth 2 tokens in
+  let value = List.nth 1 tokens in
   match first_token with
-  |"}" -> [] (*end of subnet declaration*)
-  |"option"-> (read_option tokens)::(read_subnet input_channel)
+  |"}" -> {default_lease_length = ref None;max_lease_length = ref None;scope_bottom = ref None;scope_top = ref None;parameters = ref []} (*end of subnet declaration*)
+  |"option"->
+    let new_option = read_option value in
+    let current_subnet_parameters = read_subnet input_channel in
+    current_subnet_parameters.parameters:= new_option::(!current_subnet_parameters.parameters);
+    current_subnet_parameters    
   |"range"->
+    let scope_bottom = Some (Ipaddr.V4.of_string (List.nth tokens 1)) in
+    let scope_top = Some (Ipaddr.V4.of_string (List.nth tokens 2)) in
+    let current_subnet_parameters = read_subnet input_channel in
+    current_subnet_parameters.scope_bottom:= scope_bottom;
+    current_subnet_parameters.scope_top:= scope_top;
+    current_subnet_parameters
+  |"default_lease_length" ->
+    let lease_length = Int32.of_string value in
+    let current_options = read_subnet input_channel in
+    currentoptions.default_lease_length := lease_length;
+    current_options
+  |"max_lease_length" ->
+    let lease_length = Int32.of_string(List.nth tokens 1) in
+    let current_options = read_subnet input_channel in
+    currentoptions.max_lease_length := lease_length;
+    current_options
+    
+(*
+let rec read_group input_channel = 
+  let next_line = cut_at_semicolon(input_line input_channel) in
+  let tokens = Str.split " +" next_line in
+  let first_token = List.hd tokens in
+  let value = List.nth 1 tokens in
+  match first_token with
+*)
+      
+    
+    
   
