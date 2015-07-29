fd234b39d963e0532ef2465e1d76e01263bbaba1
alistairfisher <alistairstfisher@gmail.com>
2015-07-29 13:44:31 +0100

    Fixed optional selection

--- dhcp/dhcp_serverv4_options.ml
+++ dhcp/dhcp_serverv4_options.ml
@@ -11,16 +11,34 @@
 * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 *)
 
-open Dhcpv4_option.Packet;;
+open Dhcpv4_option;;
 
-let rec parameter_request c_requests s_parameters = match c_requests with
+let t_equalto_msg t msg = 
+  let string_of_t = t_to_string t in
+  let string_of_msg = msg_to_string msg in
+  string_of_t = string_of_msg;;
+
+let rec find_option option parameter_list =
+  match parameter_list with
+  |[] -> None
+  |h::t ->
+    if t_equalto_msg h option then Some h
+    else find_option option t;;
+
+let rec parameter_request c_requests s_parameters = match c_requests with (*This will cause searches for stuff the server can't provide like Parameter Request.
+  It's still correct (provided the server parameters themselves don't contain strange parameters) but inefficient*)
   |[]->[]
-  |(h::t) -> List.assoc h s_parameters :: (parameter_request t s_parameters);;
+  |(h::t) ->
+    let option = find_option h s_parameters in
+    match option with
+    |None -> parameter_request t s_parameters
+    |Some o -> o::(parameter_request t s_parameters);;
 
 
 let make_options_lease ~client_requests ~server_parameters ~serverIP ~lease_length ~message_type =
-    (*let params = parameter_request ~c_requests:client requests ~s_parameters:parameters_list in*)
-    { op = message_type; opts= [`Lease_time lease_length;`Server_identifier serverIP;`End]};;
+  (*let params = parameter_request ~c_requests:client requests ~s_parameters:parameters_list in*)
+  { op = message_type; opts= [`Lease_time lease_length;`Server_identifier serverIP;`End]};;
     
 let make_options_no_lease ~client_requests ~server_parameters ~serverIP ~message_type =
-    {op = message_type;opts = [`Server_identifier serverIP;`End]};;
+  {op = message_type;opts = [`Server_identifier serverIP;`End]};;
+    
